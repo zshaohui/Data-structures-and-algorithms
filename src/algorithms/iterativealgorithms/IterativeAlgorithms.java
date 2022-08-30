57aed91a213217eb7f66c756c811eab5d0dd0c97
zshaohui <1365824378@qq.com>
2022-08-30 17:37:25 +0800

    递归算法--打印,阶乘问题

---
+++ src/algorithms/iterativealgorithms/IterativeAlgorithms.java
@@ -0,0 +1,37 @@
+package algorithms.iterativealgorithms;
+
+/**
+ * 迭代算法
+ */
+public class IterativeAlgorithms {
+
+    public static void main(String[] args) {
+        int result = factorial(4);
+        System.out.println(result);
+    }
+
+
+    /**
+     * 阶乘问题
+     * @param n
+     * @return
+     */
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return factorial(n - 1) * n;
+    }
+
+    /**
+     * 打印问题
+     * @param n
+     */
+    public static void print(int n) {
+        if (n > 0) {
+            print(n - 1);
+        }
+        System.out.println(n);
+    }
+
+}
