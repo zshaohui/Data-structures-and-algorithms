c7ff8aca79d531b20a9c266d292955510a3febea
zshaohui <1365824378@qq.com>
2022-09-01 11:48:31 +0800

    递归算法

--- src/algorithms/sort/Select.java
+++ src/algorithms/sort/Select.java
@@ -3,26 +3,33 @@ package algorithms.sort;
 import common.SortUtils;
 
 /**
- * 冒泡排序
+ * 选择排序
  */
-public class Bubble {
+public class Select {
+
     public static void main(String[] args) {
-        int arr[] = new int[]{-2, 3, 2};
+        int arr[] = new int[]{1, 6, 8, 9, 2, 3, 5, 4, 7};
         System.out.printf("排序前:");
         SortUtils.print(arr);
-        sort(arr); //开始排序
+        sort(arr);
         System.out.printf("排序后:");
         SortUtils.print(arr);
     }
 
     private static void sort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            int minNum = arr[i];
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                if (arr[j] < minNum) {
+                    minIndex = j;
+                    minNum = arr[j];
                 }
+            }
+            //值互换
+            if (minIndex != i) {
+                arr[minIndex] = arr[i];
+                arr[i] = minNum;
             }
         }
     }
---
+++ src/common/SortUtils.java
@@ -0,0 +1,15 @@
+package common;
+
+public class SortUtils {
+
+    /**
+     * 打印数组
+     * @param arr
+     */
+    public static void print(int[] arr) {
+        for (int i : arr) {
+            System.out.printf(i + " ");
+        }
+        System.out.println();
+    }
+}
